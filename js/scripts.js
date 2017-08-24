ba2e4546f3d6da5b41e4a3927e2e05220426d756
Riki Montgomery <mostriki820@gmail.com>
2017-08-23 22:42:14 -0700

    add completely inadequate js and jquery to play with

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,19 @@
+// front-end logic
+var nice_things = ["Veronica is beautiful!", "Veronica is full of wit", "Veronica is smarter than me."];
+
+Array.prototype.random = function() {
+  return this[Math.floor(Math.random() * this.length)];
+};
+
+function generate() {
+  var phrase = Math.floor(nice_things.length * Math.random());
+  for (var i = 0; i <= nice_things.length; i++)
+}
+
+
+// back-end logic
 $(document).ready(function() {
-  $
+  $('#click').click(function(event)) {
+    event.preventDefault();
+    var message = generate(nice_things[i])
+  });
